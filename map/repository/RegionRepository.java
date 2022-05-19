60147951c58a9d0a462515c70795726a4edd13cf
avazbekmurataliyev <97029745+avazbekmurataliyev@users.noreply.github.com>
2022-05-19 14:24:52 +0500

    Delete map directory

--- map/repository/RegionRepository.java
+++
@@ -1,7 +0,0 @@
-package uz.avazbek.map.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import uz.avazbek.map.entity.Region;
-
-public interface RegionRepository extends JpaRepository<Region,Integer> {
-}
